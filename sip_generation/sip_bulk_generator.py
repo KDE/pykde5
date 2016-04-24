7619949d9d170e17754a39ead5e13738ded9217b
Shaheed Haque <srhaque@theiet.org>
2016-04-24 15:29:19 +0100

    Remove debugs.

--- sip_generation/sip_bulk_generator.py
+++ sip_generation/sip_bulk_generator.py
@@ -118,10 +118,8 @@ class SipBulkGenerator(SipGenerator):
         #
         new_style_sips = [s for s in sip_files if not s.endswith(".sip")]
         old_style_sips = [s for s in sip_files if s.endswith(".sip")]
-        print("Before",old_style_sips, new_style_sips)
         for n in new_style_sips:
             old_style_sips = [s for s in old_style_sips if s.lower() != n.lower() + ".sip"]
-        print("After",old_style_sips, new_style_sips)
         sip_files = set(new_style_sips + old_style_sips)
         #
         # Create a SIP module including all the SIP files in this directory.
